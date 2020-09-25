da402bb7f64f675cecdeffd58861259f31eaf374
Kesho <se.hakam88@gmail.com>
2020-09-25 18:36:58 +0200

    firs code base for spring boot test , tdd and integration

---
+++ src/main/java/com/testing/payment/CardPaymentCharger.java
@@ -0,0 +1,15 @@
+package com.testing.payment;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+@Service
+public interface CardPaymentCharger {
+
+    CardPaymentCharge chargeCard(
+            String cardSource,
+            BigDecimal amount,
+            Currency currency,
+            String description
+    );
+}
---
+++ src/main/java/com/testing/payment/Currency.java
@@ -0,0 +1,7 @@
+package com.testing.payment;
+
+public enum Currency {
+    USD,
+    GBP,
+    EUR
+}
